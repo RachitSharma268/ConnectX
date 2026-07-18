bdaa1d71000998183c15202d8adca197b6476684
Rachit Sharma <rachitseema01@gmail.com>
2026-07-18 12:16:26 +0530

    Authentication page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import Landing from "../pages/landing";
+import Authentication from "../pages/authentication";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/auth" element={<Authentication />} />
         </Routes>
       </Router>
     </>
